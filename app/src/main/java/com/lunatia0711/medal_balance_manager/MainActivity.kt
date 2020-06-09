d6849cd4ecb3f5bba0ef0c4d386c71ee2e31a8a8
Lunatia0711 <lunatia0711@gmail.com>
2020-06-09 23:30:48 +0900

    Add AdMob

--- app/src/main/java/com/lunatia0711/medal_balance_manager/MainActivity.kt
+++ app/src/main/java/com/lunatia0711/medal_balance_manager/MainActivity.kt
@@ -2,10 +2,20 @@ package com.lunatia0711.medal_balance_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 }
