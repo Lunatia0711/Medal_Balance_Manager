cc2ef791458f3cc04312ced2538ac0e3fa5852a7
Lunatia0711 <lunatia0711@gmail.com>
2020-06-17 21:22:05 +0900

    Add Layout

--- app/src/main/java/com/lunatia0711/medal_balance_manager/MainActivity.kt
+++ app/src/main/java/com/lunatia0711/medal_balance_manager/MainActivity.kt
@@ -6,10 +6,17 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
+import android.text.format.DateFormat
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+    , DatePickerFragment.OnDateSelectedListener {
 
-    lateinit var mAdView : AdView
+    lateinit var mAdView: AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +24,24 @@ class MainActivity : AppCompatActivity() {
         mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
+
+        val today = Calendar.getInstance().time
+        dateText.text = DateFormat.format("yyyy/MM/dd", today)
+
+        dateText.setOnClickListener {
+            val dialog = DatePickerFragment()
+            dialog.show(supportFragmentManager, "date_dialog")
+        }
+        val storeList : List<String> = listOf("シルクハット","アドアーズ","モーリーファンタジー")
+        val adpt0 = ArrayAdapter(applicationContext,android.R.layout.simple_spinner_item,storeList)
+        adpt0.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        storeSpinner.adapter = adpt0
     }
+
+    override fun onSelected(year: Int, month: Int, date: Int) {
+        val c = Calendar.getInstance()
+        c.set(year, month, date)
+        dateText.text = DateFormat.format("yyyy/MM/dd", c)
+    }
+
 }
